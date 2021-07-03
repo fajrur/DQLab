9677f59a5c19f690c9faa58a9f834fd83a632467
Fajrur Rahman Suprapto <rurjaf@gmail.com>
2021-07-03 16:19:07 +0700

    Kelas Persiapan

---
+++ Modules/01 R/01 Kelas Persiapan/01 Introduction to Data Science with R.r
@@ -0,0 +1,27 @@
+# "Hello World" (https://academy.dqlab.id/main/livecode/111/211/1059?pr=0)
+"Hello World"
+
+
+# "Praktek Angka dan Perhitungan" (https://academy.dqlab.id/main/livecode/111/211/1062?pr=0)
+10 + 7
+
+
+# "Variable dalam R" (https://academy.dqlab.id/main/livecode/111/211/1060?pr=0)
+a = 5
+print(a)
+
+
+# "Praktik Penggunaan Comment" (https://academy.dqlab.id/main/livecode/111/212/1064?pr=0)
+10 + 7 # Ini adalah baris komentar
+
+
+# "Praktek Penggunaan Vector - Bagian Satu" (https://academy.dqlab.id/main/livecode/111/212/1066?pr=0)
+c(3, 10, 15)
+
+
+# "Praktek Penggunaan Vector - Bagian Dua" (https://academy.dqlab.id/main/livecode/111/212/1067?pr=0)
+c(1:5)
+
+
+# "Menggunakan Fungsi Summary" (https://academy.dqlab.id/main/livecode/111/212/1070?pr=0)
+summary(c(1:5))
